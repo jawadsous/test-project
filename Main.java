1f09f37679bdab6d683e48b90c35bdf2313f8bd0
ACER <jawads2015@gmail.com>
2024-12-26 07:55:40 +0300

    edit new tracher

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         Teacher teacher1= new Teacher("jawad edited new ",25,"damascus",4);
+        Teacher teacher2= new Teacher("eiad edited new ",25,"damascus",4);
         teacher1.addSubject("math");
 
 
