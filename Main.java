09dfdc3ea0f0eadab7688a092b4aac1c1041c836
ACER <jawads2015@gmail.com>
2024-12-26 08:14:03 +0300

    nnn

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
         Teacher teacher1= new Teacher("jawad edited new ",25,"damascus",4);
         Teacher teacher2= new Teacher("eiad edited new ",25,"damascus",4);
         teacher1.addSubject("math");
-
+ddddd
 
     }
     
