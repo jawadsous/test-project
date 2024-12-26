31e3bc24a8b367e2e63cce9934a9d9ccf56c3132
‪jawad alsous‬‏ <jawadalsousbs@gmail.com>
2024-12-26 09:18:41 +0300

    Update Main.java
    delete teacher 2

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         Teacher teacher1= new Teacher("jawad edited new ",25,"damascus",4);
-        Teacher teacher2= new Teacher("eiad edited new ",25,"damascus",4);
+        
         Teacher teacher3= new Teacher("ddd edited new ",25,"damascus",4);
         Teacher teacher4= new Teacher("aaa edited new ",25,"damascus",4);
         teacher1.addSubject("math and alot of things ");
