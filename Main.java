5c69d3cf26fff5db57ebfd9192ff8b0a693fa756
ACER <jawads2015@gmail.com>
2024-12-26 08:19:46 +0300

    4 teachers

--- Main.java
+++ Main.java
@@ -3,8 +3,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Teacher teacher1= new Teacher("jawad edited new ",25,"damascus",4);
         Teacher teacher2= new Teacher("eiad edited new ",25,"damascus",4);
+        Teacher teacher3= new Teacher("ddd edited new ",25,"damascus",4);
+        Teacher teacher4= new Teacher("aaa edited new ",25,"damascus",4);
         teacher1.addSubject("math");
-ddddd
+
 
     }
     
