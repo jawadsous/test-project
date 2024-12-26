87a30e9b1c7390b146175297b3f414e1579348d0
ACER <jawads2015@gmail.com>
2024-12-26 07:35:46 +0300

    new java and test file for test

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Teacher teacher1= new Teacher("ahmad",25,"damascus",4);
+        teacher1.addSubject("math");
+
+
+    }
+    
+}
